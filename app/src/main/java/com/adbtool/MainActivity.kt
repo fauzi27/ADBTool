3f85d3971e7ae37a5c33aca600b39eccf6ba8443
fauzi27 <44411728+fauzi27@users.noreply.github.com>
2026-03-27 05:18:05 +0700

    update app/src/main/java/com/adbtool/MainActivity.kt

--- app/src/main/java/com/adbtool/MainActivity.kt
+++ app/src/main/java/com/adbtool/MainActivity.kt
@@ -2,26 +2,25 @@ package com.adbtool
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var webView: WebView
-
-    @SuppressLint("SetJavaScriptEnabled")
+    @SuppressLint("SetJavaScriptEnabled", "AddJavascriptInterface")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        webView = WebView(this)
+        val webView = WebView(this).apply {
+            settings.javaScriptEnabled = true
+            settings.allowFileAccess = true
+            settings.domStorageEnabled = true
+            webViewClient = WebViewClient()
+            webChromeClient = WebChromeClient()
+            addJavascriptInterface(WebAppInterface(this@MainActivity), "Android")
+        }
         setContentView(webView)
-
-        webView.settings.javaScriptEnabled = true
-        webView.webViewClient = WebViewClient()
-
-        webView.addJavascriptInterface(WebAppInterface(this), "Android")
-
         webView.loadUrl("file:///android_asset/web/index.html")
     }
 }
